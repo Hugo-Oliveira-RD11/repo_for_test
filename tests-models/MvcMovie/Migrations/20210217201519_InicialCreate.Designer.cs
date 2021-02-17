02ed35b8c42ca6477eccc7b1e55406c746f43fd0
hugo de oliveira <hugo.resennde1123@gmail.com>
2021-02-17 18:09:48 -0300

    adicionando um novo campo no banco de dados

--- tests-models/MvcMovie/Migrations/20210217201519_InicialCreate.Designer.cs
+++ tests-models/MvcMovie/Migrations/20210217201519_InicialCreate.Designer.cs
@@ -3,15 +3,17 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using MvcMovie.Data;
 
 namespace MvcMovie.Migrations
 {
     [DbContext(typeof(MvcMovieContext))]
-    partial class MvcMovieContextModelSnapshot : ModelSnapshot
+    [Migration("20210217201519_InicialCreate")]
+    partial class InicialCreate
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
