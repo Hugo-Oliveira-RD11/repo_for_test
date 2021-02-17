666e5bb35739d037b6aef5dbf1917a294f4abc35
hugo de oliveira <hugo.resennde1123@gmail.com>
2021-02-16 21:18:59 -0300

    adicionando um filtro de genero e de nome

--- tests-models/MvcMovie/Controllers/MoviesController.cs
+++ tests-models/MvcMovie/Controllers/MoviesController.cs
@@ -20,9 +20,28 @@ namespace MvcMovie.Controllers
         }
 
         // GET: Movies
-        public async Task<IActionResult> Index()
+        public async Task<IActionResult> Index(string movieGenre,string searchString)
         {
-            return View(await _context.Movie.ToListAsync());
+
+            IQueryable<string> genreQuery= from m in _context.Movie orderby m.Genre select m.Genre;
+            
+            var movies = from m in _context.Movie select m;
+
+
+            if(!String.IsNullOrEmpty(searchString)){
+                movies=movies.Where(s=>s.Title.Contains(searchString));
+            }
+
+            if(!String.IsNullOrEmpty(movieGenre)){
+                movies=movies.Where(x=>x.Genre == movieGenre);
+            }
+            var movieGenreVm=new MovieGenreViewModel()
+            {
+                Genres =new SelectList(await genreQuery.Distinct().ToListAsync()),
+                Movies =await movies.ToListAsync()
+            };
+
+            return View(movieGenreVm);
         }
 
         // GET: Movies/Details/5
