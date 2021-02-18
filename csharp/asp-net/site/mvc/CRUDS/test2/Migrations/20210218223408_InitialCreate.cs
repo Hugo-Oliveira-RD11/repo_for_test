4f79083cabb54474ea58ebce0004db5b9e3a0b3b
hugo de oliveira <hugo.resennde1123@gmail.com>
2021-02-18 19:46:19 -0300

    dando o update no banco de dados(ou seja, criando de verdade as tabelas no banco de dados)

---
+++ csharp/asp-net/site/mvc/CRUDS/test2/Migrations/20210218223408_InitialCreate.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace test2.Migrations
+{
+    public partial class InitialCreate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Users",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    NomeR = table.Column<string>(type: "nvarchar(60)", maxLength: 60, nullable: false),
+                    NomeU = table.Column<string>(type: "nvarchar(30)", maxLength: 30, nullable: false),
+                    Email = table.Column<string>(type: "nvarchar(60)", maxLength: 60, nullable: false),
+                    senha = table.Column<string>(type: "nvarchar(40)", maxLength: 40, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Users", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Users");
+        }
+    }
+}
