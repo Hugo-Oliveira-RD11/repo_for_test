e5204f2046248d454a4af1c504c9dcafdd5562b1
hugo de oliveira <hugo.resennde1123@gmail.com>
2021-02-19 17:57:48 -0300

    arrumando os nomes das colunas, 'NomeR' para 'Seu nome Real' e 'NomeU' para 'Seu nome Virtual'

--- csharp/asp-net/site/mvc/CRUDS/test2/Models/CreateUser.cs
+++ csharp/asp-net/site/mvc/CRUDS/test2/Models/CreateUser.cs
@@ -1,14 +1,17 @@
 using System;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace test2.Models{
     public class CreateUser{
         public int Id{get;set;}
         [Required]
         [StringLength(60, MinimumLength = 3)]
+        [Display(Name="Seu nome Real")]
         public string NomeR{get;set;}
         [Required]
         [StringLength(30, MinimumLength = 3)]
+        [Display(Name="Seu nome Virtual")]
         public string NomeU{get;set;}
         [Required]
         [StringLength(60, MinimumLength = 3)]
