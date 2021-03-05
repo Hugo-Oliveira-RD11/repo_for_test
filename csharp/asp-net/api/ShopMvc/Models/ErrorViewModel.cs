678b1f1d9832a9ef5abca30295d1e28144585080
hugo de oliveira <hugo.resennde1123@gmail.com>
2021-03-04 21:50:35 -0300

    Criando o projeto mvc com autenticacao e login, logout e register(scaffold)

---
+++ csharp/asp-net/api/ShopMvc/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace ShopMvc.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
