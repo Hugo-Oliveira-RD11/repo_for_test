fc0ed36017163709685f080be48ed4de7175ad27
hugo de oliveira <hugo.resennde1123@gmail.com>
2021-03-12 23:20:43 -0300

    ajeitando o ApplicationDataContext para O identity entender quem e o banco de dados

--- csharp/asp-net/site/mvc/TestAuth/Models/Data/ApplicationDataContext.cs
+++ csharp/asp-net/site/mvc/TestAuth/Models/Data/ApplicationDataContext.cs
@@ -1,8 +1,11 @@
+using System;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
 namespace TestAuth.Models.Data
 {
-    public class ApplicationDataContext : DbContext
+    public class ApplicationDataContext : IdentityDbContext<ApplicationUser,IdentityRole<Guid>,Guid>
     {
        public ApplicationDataContext(DbContextOptions<ApplicationDataContext> options):base(options){
 
