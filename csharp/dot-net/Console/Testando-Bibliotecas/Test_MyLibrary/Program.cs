0c2335846a52153530c3f2768e07340e304294a7
hugo de oliveira <hugo.resennde1123@gmail.com>
2021-04-28 17:20:03 -0300

    Criando mais um script besta(mas funciona)

---
+++ csharp/dot-net/Console/Testando-Bibliotecas/Test_MyLibrary/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using MyLibrary;
+
+namespace Test_MyLibrary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Digite o seu nome :");
+            
+            string nome = Console.ReadLine();
+
+            Console.WriteLine(Book1.Upper(nome));
+
+        }
+    }
+}
