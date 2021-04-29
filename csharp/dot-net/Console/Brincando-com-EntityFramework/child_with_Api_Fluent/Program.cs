6f2d4a58425248a9c522a131760bb1c7b1f00480
hugo de oliveira <hugo.resennde1123@gmail.com>
2021-04-29 16:54:55 -0300

    Criando um novo projeto (eu acho)

---
+++ csharp/dot-net/Console/Brincando-com-EntityFramework/child_with_Api_Fluent/Program.cs
@@ -0,0 +1,20 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+
+namespace child_with_Api_Fluent
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+
+        }
+
+    }
+}
