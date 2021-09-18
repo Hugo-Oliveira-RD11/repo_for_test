7415b2aec461834059ce5bf32a85b8b925dfb3c3
Hugo de Oliveira Resende <hugo.resennde1123@gmail.com>
2021-09-18 12:15:57 -0300

    mudando a organizacao dos diretorios

---
+++ Web/bricando-com-Identity/OverviewIdentity/Id.Overview.Mvc/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,21 @@
+using System;
+using Id.Overview.Mvc.Data;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+[assembly: HostingStartup(typeof(Id.Overview.Mvc.Areas.Identity.IdentityHostingStartup))]
+namespace Id.Overview.Mvc.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+            });
+        }
+    }
+}
