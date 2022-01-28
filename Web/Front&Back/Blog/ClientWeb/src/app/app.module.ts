f4ae28a888518e17b07b2f9704a558367fad70b2
Hugo de Oliveira Resende <hugo.resennde1123@gmail.com>
2022-01-28 17:10:03 -0300

    Created footer component

--- Web/Front&Back/Blog/ClientWeb/src/app/app.module.ts
+++ Web/Front&Back/Blog/ClientWeb/src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MenuComponent } from './component/menu/menu.component';
+import { FooterComponent } from './component/footer/footer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    MenuComponent
+    MenuComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
