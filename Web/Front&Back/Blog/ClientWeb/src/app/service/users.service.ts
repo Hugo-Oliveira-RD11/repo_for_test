29a834a47c871a1bc27e2ed6262f52e8396755fe
Hugo de Oliveira Resende <hugo.resennde1123@gmail.com>
2022-01-29 19:03:03 -0300

    modified users services

--- Web/Front&Back/Blog/ClientWeb/src/app/service/users.service.ts
+++ Web/Front&Back/Blog/ClientWeb/src/app/service/users.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import { HttpClient, HttpErrorResponse, HttpHeaders } from '@angular/common/http';
 import { Users } from '../models/users';
 import { API_PATH } from 'src/environments/environment';
+import { catchError, Observable, retry, throwError } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -10,7 +11,27 @@ export class UsersService {
 
   constructor(private httpClient: HttpClient) { }
 
-  GetAll(){
-    return this.httpClient.get<Users[]>(`${API_PATH}/AllUsers`).toPromise();
+  httpOptions = {
+    headers: new HttpHeaders({ 'Content-Type': 'application/json' })
   }
+  GetAll(): Observable<Users[]> {
+    return this.httpClient.get<Users[]>(`${API_PATH}/AllUsers`)
+      .pipe(
+        retry(2),
+        catchError(this.handleError))
+  }
+  handleError(error: HttpErrorResponse) {
+    let errorMessage = '';
+    if (error.error instanceof ErrorEvent) {
+      // Erro ocorreu no lado do client
+      errorMessage = error.error.message;
+    } else {
+      // Erro ocorreu no lado do servidor
+      errorMessage = `Código do erro: ${error.status}, ` + `menssagem: ${error.message}`;
+    }
+    console.log(errorMessage);
+    return throwError(errorMessage);
+  };
+
+
 }
